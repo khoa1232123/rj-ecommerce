7ebf321bd8cfebf64e423d94279583938e2be716
khoa1232123 <38268470+khoa1232123@users.noreply.github.com>
2022-01-23 23:41:06 +0700

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
+import { Link } from 'react-router-dom';
+
+import Grid from './Grid';
+
 const Footer = () => {
-  return <div>Footer Footer</div>;
+  return <div></div>;
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/images/Logo-2.png';
 
 const mainNav = [
@@ -21,8 +22,33 @@ const mainNav = [
 ];
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const activeNav = mainNav.findIndex((e) => e.path === pathname);
+
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add('shrink');
+      } else {
+        headerRef.current.classList.remove('shrink');
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
+  const menuLeft = useRef(null);
+
+  const menuToggle = () => menuLeft.current.classList.toggle('active');
+
   return (
-    <div className="header">
+    <div className="header" ref={headerRef}>
       <div className="container">
         <div className="header__logo">
           <Link to="/">
@@ -30,17 +56,19 @@ const Header = () => {
           </Link>
         </div>
         <div className="header__menu">
-          <div className="header__menu__mobile--toggle">
+          <div className="header__menu__mobile--toggle" onClick={menuToggle}>
             <i className="bx bx-menu-alt-left"></i>
           </div>
-          <div className="header__menu__left">
-            <div className="header__menu__left__close">
-              <i className="bx bx-chevron"></i>
+          <div className="header__menu__left" ref={menuLeft}>
+            <div className="header__menu__left__close" onClick={menuToggle}>
+              <i className="bx bx-chevron-left"></i>
             </div>
             {mainNav.map((item, index) => (
               <div
                 key={index}
-                className="header__menu__item header__menu__left__item"
+                className={`header__menu__item header__menu__left__item ${
+                  index === activeNav ? 'active' : ''
+                }`}
               >
                 <Link to={item.path}>
                   <span>{item.display}</span>
